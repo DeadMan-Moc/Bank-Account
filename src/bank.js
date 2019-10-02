896499c7d25aaff1a5e548b885d5561f3857d901
DeadMan-MOC <mokgohloallie@gmail.com>
2019-10-02 11:29:16 +0200

    --- Auto-Git Commit ---

--- src/bank.js
+++ src/bank.js
@@ -11,9 +11,46 @@ class Bank{
           if (bankAccountNumber == a){
               this.balance = this.balance - amount
               return this.balance;
-            }
+        }
           else{
-            console.error("fUCK BANK");
+            console.error("Your bank account has insufficient funds");
+          }
+  
+        }
+  
+      //   else{
+      //     return "Insufficient balance";
+      //   }
+      // }
+    }
+    deposit(bankAccountNumber, amount){
+      if (bankAccountNumber !== undefined){
+        return (this.balance = this.balance + amount);
+      }
+    }
+    //transfer(amount)
+  }
+  
+  let bank = new Bank(1930647846, 1000);
+  console.log(bank.deposit(1930647846, 1900));
+  console.log(bank.withdraw(1930647846, 100));
+  // console.log(bank.withdraw(1930647843, 1000))
+class Bank{
+    constructor(bankAccountNumber, amount){
+      this.balance = amount;
+      this.account = bankAccountNumber;
+    }
+    withdraw(bankAccountNumber, amount){
+      if (bankAccountNumber !== undefined){
+        // if (amount <= this.balance){
+          // this.amount = amount;
+          var a = this.account
+          if (bankAccountNumber == a){
+              this.balance = this.balance - amount
+              return this.balance;
+        }
+          else{
+            console.error("Your bank account has insufficient funds");
           }
   
         }
